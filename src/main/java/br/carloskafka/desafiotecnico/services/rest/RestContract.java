4bff658fbe812e6ede01c34e750b2c7924b5ba6d
carloskafka7@gmail.com <carloskafka7@gmail.com>
2019-04-16 19:08:28 -0300

    Update RestContract.java

--- src/main/java/br/carloskafka/desafiotecnico/services/rest/RestContract.java
+++ src/main/java/br/carloskafka/desafiotecnico/services/rest/RestContract.java
@@ -1,12 +1,12 @@
 package br.carloskafka.desafiotecnico.services.rest;
 
 public class RestContract {
-	public static final String PARAMETER_INFO_ROUTE = "text";
+	public static final String PARAMETER_INFO_ROUTE = "inforoute";
 	public static final String URL_PARAMETER_INFOROUTE = "{" + PARAMETER_INFO_ROUTE + "}";
 
 	public static final String URL_BASE = "http://localhost:8080";
 	
-	public static final String URL_ROUTES = "/";
-	public static final String URL_FETCH_BEST_ROUTE = "/" + URL_PARAMETER_INFOROUTE;
+	public static final String URL_ROUTES = "/route";
+	public static final String URL_FETCH_BEST_ROUTE = URL_ROUTES + "/best-route/" + URL_PARAMETER_INFOROUTE;
 
 }
